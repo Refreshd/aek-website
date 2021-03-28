30929cd0011b14b10184fb31d470e9f91fe311f6
Zoran Zlokapa <z.zoran@gmail.com>
2021-03-28 18:24:55 +0200

    Added new site markup and styles

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
+  return {
+    templateFormats: ['md', 'njk', 'html'],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: '/',
+
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    dir: {
+      input: 'src',
+    },
+  };
+};
